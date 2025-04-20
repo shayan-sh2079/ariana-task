c765874cd5e1cb3e1ba2cec14760e046564489a3
sh2079 <shayan.sh20079@gmail.com>
2025-04-20 21:52:34 +0330

    feat: Defined tweets res types

--- src/types/responses.d.ts
+++ src/types/responses.d.ts
@@ -1,3 +1,12 @@
+export interface PaginationRes<T> {
+  count: number;
+  total_pages: number;
+  next: null | string;
+  previous: null | string;
+  current_page: number;
+  results: T[];
+}
+
 export interface ProfileRes {
   username: string;
   first_name?: string;
@@ -8,3 +17,18 @@ export interface ProfileRes {
 export interface AuthRes {
   token: string;
 }
+
+interface TweetRes {
+  id: number;
+  has_edit_permission: boolean;
+  created_at: string;
+  text: string;
+  author: {
+    username: string;
+    first_name: string;
+    last_name: string;
+    avatar: string;
+  };
+}
+
+export type TweetsRes = PaginationRes<TweetRes>;
